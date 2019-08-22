e472ce267ecbf3eb176680cfb55b07076aa864d4
medoo <saad_43@outlook.com>
2019-08-22 08:15:38 +0200

    update the image

--- plugins/main.js
+++ plugins/main.js
@@ -11,8 +11,10 @@ $(document).ready(function(){
     }
 
     function onBackKeyDown() {
-		cordova.exec(null, null, "InAppBrowser", "injectScriptCode", ["history.back()"]);
-
+		//cordova.exec(null, null, "InAppBrowser", "injectScriptCode", ["history.back()"]);
+		var previouspage = window.sessionStorage.getItem("page");
+		var currentPage = window.location.href;
+		window.sessionStorage.setItem("page",currentPage);
 	}
 	
 											
