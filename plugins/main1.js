9c48db523a481eabaeb181762aaddbd4ae2d6da6
medoo <saad_43@outlook.com>
2019-09-25 08:04:17 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.html

--- plugins/main1.js
+++ plugins/main1.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
 				 		
 	$("body").fadeIn(6000);
-	
-	document.addEventListener("deviceready", onDeviceReady, false);
-	//window.location.replace('index.html');
-	window.location="index.html";
 
+	var currentPage = window.location.href;
+	window.sessionStorage.setItem("index.html",currentPage);
 
 
 
